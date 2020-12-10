bbf1d0d56e785d4997a4c8f54a525c88504ffec2
james <engrjayt200@gmail.com>
2020-12-10 19:27:28 +0100

    File system module

--- app.js
+++ app.js
@@ -20,7 +20,43 @@ class Person extends EventEmitter{
 let pedro = new Person("Pedro");
 
 pedro.on("name", ()=>{
-    console.log("My name is" + pedro.name)
+    //console.log("My name is" + pedro.name)
 })
 
 pedro.emit("name")
+
+// Readline module
+const readline = require("readline");
+const { ltr } = require("semver");
+const rl = readline.createInterface({input: process.stdin, output: process.stdout});
+
+let num1 = Math.floor((Math.random() *10) +1);
+let num2 = Math.floor(Math.random() * 10 + 1);
+
+const answer = num1 + num2;
+
+rl.question(`What is ${num1} + ${num2} ?  \n`, 
+(userInput)=>{
+ if (userInput.trim() == answer) {
+   rl.close();
+ } else {
+     rl.setPrompt('Response incorrect, pls try again');
+     rl.prompt();
+     rl.on('line', (userInput)=>{
+         if(userInput.trim() == answer)
+         rl.close();
+         else{
+             rl.setPrompt(`Your answer of ${userInput} is incorrect try again\n`)
+             rl.prompt();
+         }
+
+     })
+ }
+
+})
+
+rl.on("close", ()=>{
+    console.log("Correct !!!");
+
+} 
+)
