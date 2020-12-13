70f045bb1c70498a9e6f53baace55f5393c969e6
james <engrjayt200@gmail.com>
2020-12-13 11:42:17 +0100

    Serving static files using express

--- npmfile/index.js
+++ npmfile/index.js
@@ -1,20 +1,37 @@
-const express = require('express');
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+// app.get('/', (req, res)=>{
+//     res.send("I am in Nigeria")
+// })
+// app.listen(port, ()=>{
+//     console.log(`Sever listening on port ${port}`);
+// })
+
+// app.get("/example", (req, res) => {
+//   res.send("Hitting example route");
+// });
+
+
+// Using params
+// app.get('/example/:name/:age', (req, res)=>{
+//     console.log((req.params));
+//     console.log(req.query)
+//     res.send(req.params.name + ':' + req.params.age);
+// })
+
+// SERVING STATIC FILES
+const express = require("express");
+const path = require('path');
 const app = express();
 const port = 3000;
-app.get('/', (req, res)=>{
-    res.send("I am in Nigeria")
-})
-app.listen(port, ()=>{
-    console.log(`Sever listening on port ${port}`);
-})
 
-app.get("/example", (req, res) => {
-  res.send("Hitting example route");
+app.use('/public',express.static(path.join(__dirname, 'static')) )
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'static', 'index.html'));
+});
+app.listen(port, () => {
+  console.log(`Sever listening on port ${port}`);
 });
 
-app.get('/example/:name/:age', (req, res)=>{
-    console.log((req.params));
-    console.log(req.query)
-    res.send(req.params.name + ':' + req.params.age);
-})
 
