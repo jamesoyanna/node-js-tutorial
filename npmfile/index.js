3d27f6b76c2cb4742dbc5339daa20c02b84d100d
james <engrjayt200@gmail.com>
2020-12-13 12:01:06 +0100

    Making http request with the body parser module

--- npmfile/index.js
+++ npmfile/index.js
@@ -22,16 +22,24 @@
 
 // SERVING STATIC FILES
 const express = require("express");
+const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
 const port = 3000;
 
 app.use('/public',express.static(path.join(__dirname, 'static')) )
+app.use(bodyParser.urlencoded({extended: false}))
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, 'static', 'index.html'));
 });
 app.listen(port, () => {
   console.log(`Sever listening on port ${port}`);
 });
+
+app.post('/', (req, res)=>{
+    console.log(req.body)
+// Database work here
+    res.send("Form successuflly submitted")
+})
 
 
