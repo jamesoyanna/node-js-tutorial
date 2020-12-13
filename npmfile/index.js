429e338993d6f54989138f0747b3ff9047c4f349
james <engrjayt200@gmail.com>
2020-12-13 13:05:24 +0100

    Working with json data and express

--- npmfile/index.js
+++ npmfile/index.js
@@ -29,6 +29,7 @@ const port = 3000;
 
 app.use('/public',express.static(path.join(__dirname, 'static')) )
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, 'static', 'index.html'));
 });
@@ -39,7 +40,7 @@ app.listen(port, () => {
 app.post('/', (req, res)=>{
     console.log(req.body)
 // Database work here
-    res.send("Form successuflly submitted")
+    res.json({success: true})
 })
 
 
